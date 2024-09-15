73783d76b9dfc352ea262d3ca9e462a8a17174d9
jingpingxie <jason.xie@95isee.com>
2024-09-15 19:34:54 +0800

    dev

---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class ShipController: MonoBehaviour
+    {
+
+        void Start()
+        {
+
+        }
+        void doLoadPlan(int containerCount,int danger,int normal) {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
